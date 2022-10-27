1b58459b46f3fe8ec6a51bd7649d604c8d0aa192
unknown <myokokooo911@gmail.com>
2022-10-28 01:05:35 +0630

    01:16 28 Oct 2022

--- js/app.js
+++ js/app.js
@@ -1,7 +1,39 @@
-const accordions = document.querySelectorAll('.accordion');
+//NavBar
+const navbar = document.querySelector('.navbar');
 
-accordions.forEach((accordion)=>{
-    accordion.addEventListener('click',()=>{
-        accordion.classList.toggle('active')
-    })
+window.addEventListener('scroll',()=>{
+    const getscrollY = window.scrollY;
+
+    if(getscrollY >= 250){
+        navbar.classList.add('navmenus');
+    }else{
+        navbar.classList.remove('navmenus');
+    }
 })
+//Accordion
+const accrodHeaders = document.querySelectorAll(".accord-header");
+accrodHeaders.forEach((ach) => {
+  ach.addEventListener('click',toggleItem,false)
+});
+
+function toggleItem() {
+  const currentContentEle = this.nextElementSibling;
+
+  const isCollapsed = currentContentEle.classList.contains("collapse");
+
+  accrodHeaders.forEach((ach) => {
+    const contentEle = ach.nextElementSibling;
+    if (!contentEle.classList.contains("collapse")) {
+      contentEle.classList.add("collapse");
+    }
+  });
+
+  if (isCollapsed) {
+    currentContentEle.classList.remove("collapse");
+  }
+}
+
+//Footer date and time
+let years = document.getElementById('year');
+let dates = new Date().getUTCFullYear();
+years.textContent = dates;
